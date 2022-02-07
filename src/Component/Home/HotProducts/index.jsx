7a7b8ed25dedce900bef589f0424801df955048d
namnp-1182 <nguyen.phuong.nam-c@sun-asterisk.com>
2022-02-07 17:07:27 +0700

    task.45590

--- src/Component/Home/HotProducts/index.jsx
+++ src/Component/Home/HotProducts/index.jsx
@@ -1,38 +1,36 @@
 import ProductItem from "../../ProductItem";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import "./style.scss";
+import "../Products/style.scss";
 import { Container } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { getProducts } from "../../../store/Slide/ProductsSlide";
+import { getHotProducts } from "../../../store/Slide/ProductsSlide";
 
-function Products() {
+function HotProducts() {
   const { t } = useTranslation();
   const dispatch = useDispatch();
-  const productsList = useSelector((state) => state.products.productsList.data);
-  const filter = useSelector((state) => state.products.filter);
+  const hotProduct = useSelector((state) => state.products.hot.list.data);
+  const filter = useSelector((state) => state.products.hot.filter);
 
   useEffect(() => {
-    dispatch(getProducts(filter));
+    dispatch(getHotProducts(filter));
   }, [filter, dispatch]);
 
   return (
     <section className="product-homepage">
       <Container>
-        <h3 className="product-homepage__title">New Arrivals</h3>
+        <h3 className="product-homepage__title">{t("Hot Products")}</h3>
         <div className="justify-content-center mb-2 row">
           <div className="col-sm-8">
             <p className="text-center text-muted mb-4 product-homepage__text">
-              Check out our new furniture collection! Cozy sofa, fancy chair,
-              wooden casket, and many more. The new collection brings an
-              informal elegance to your home.
+              {t("Check out our new furniture collection!")}
             </p>
           </div>
         </div>
         <div className="product-homepage__main">
-          {productsList &&
-            productsList.map((item) => (
+          {hotProduct &&
+            hotProduct.map((item) => (
               <div key={item.id} className="mb-4 col-12 col-sm-6 col-md-3">
                 <ProductItem data={item} />
               </div>
@@ -45,4 +43,4 @@ function Products() {
     </section>
   );
 }
-export default Products;
+export default HotProducts;
